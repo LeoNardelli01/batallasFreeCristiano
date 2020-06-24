a731cdbcc72fac3c870457fcea3b982a47435247
Leonel Nardelli <leonardelli@MacBook-Pro-de-Leonel.local>
2020-06-24 16:53:10 -0300

    Terminada y funcional, resta sweet alert

--- js/resultados.js
+++ js/resultados.js
@@ -1,6 +1,138 @@
 $(function(){
   var p1 = localStorage.getItem('p1');
   var p2 = localStorage.getItem('p2');
+  $("#p1").html(p1);
+  $("#p1").css('color', 'blue');
+  $("#p2").html(p2);
+  $("#p2").css('color', 'red');
 
-  
+  $("#btn-nuevaBatalla").click(function(){
+    $(location).attr('href', 'index.html');
+  });
+
+  var p1_totalPatrones = parseInt(localStorage.getItem('p1_totalPatrones'));
+  var p1_tecnica = parseInt(localStorage.getItem('p1_tecnica'));
+  var p1_flow = parseInt(localStorage.getItem('p1_flow'));
+  var p1_punch = parseInt(localStorage.getItem('p1_punch'));
+  var p1_dobletempo = parseInt(localStorage.getItem('p1_dobletempo'));
+  var p1_escena = parseInt(localStorage.getItem('p1_escena'));
+
+  var p1_total = p1_totalPatrones + p1_tecnica + p1_flow + p1_punch + p1_dobletempo + p1_escena;
+
+
+  var p2_totalPatrones = parseInt(localStorage.getItem('p2_totalPatrones'));
+  var p2_tecnica = parseInt(localStorage.getItem('p2_tecnica'));
+  var p2_flow = parseInt(localStorage.getItem('p2_flow'));
+  var p2_punch = parseInt(localStorage.getItem('p2_punch'));
+  var p2_dobletempo = parseInt(localStorage.getItem('p2_dobletempo'));
+  var p2_escena = parseInt(localStorage.getItem('p2_escena'));
+
+  var p2_total = p2_totalPatrones + p2_tecnica + p2_flow + p2_punch + p2_dobletempo + p2_escena;
+
+  $("#p1_resPatrones").html(p1_totalPatrones);
+  $("#p1_resTecnica").html(p1_tecnica);
+  $("#p1_resFlow").html(p1_flow);
+  $("#p1_resPunch").html(p1_punch);
+  $("#p1_resDobletempo").html(p1_dobletempo);
+  $("#p1_resEscena").html(p1_escena);
+
+  $("#p2_resPatrones").html(p2_totalPatrones);
+  $("#p2_resTecnica").html(p2_tecnica);
+  $("#p2_resFlow").html(p2_flow);
+  $("#p2_resPunch").html(p2_punch);
+  $("#p2_resDobletempo").html(p2_dobletempo);
+  $("#p2_resEscena").html(p2_escena);
+
+  $("#p1_total").html(p1_total);
+  $("#p2_total").html(p2_total);
+
+
+  if (p1_total > p2_total) {
+    $("body").css('background', 'blue');
+    $("#resultado").html("Ganó " + p1);
+  } else if (p1_total < p2_total){
+    $("body").css('background', 'red');
+    $("#resultado").html("Ganó " + p2);
+  } else{
+    $("#resultado").html('Réplica');
+  }
+
+
+  // porcentajes para los graficos
+  var p1_porcentajePatrones = p1_totalPatrones * 100 / 24;
+  var p1_porcentajeTecnica = p1_tecnica * 100 / 2;
+  var p1_porcentajeFlow = p1_flow * 100 / 2;
+  var p1_porcentajePunch = p1_punch * 100 / 2;
+  var p1_porcentajeDobletempo = p1_dobletempo * 100 / 2;
+  var p1_porcentajeEscena = p1_escena * 100 / 2;
+
+  var p2_porcentajePatrones = p2_totalPatrones * 100 / 24;
+  var p2_porcentajeTecnica = p2_tecnica * 100 / 2;
+  var p2_porcentajeFlow = p2_flow * 100 / 2;
+  var p2_porcentajePunch = p2_punch * 100 / 2;
+  var p2_porcentajeDobletempo = p2_dobletempo * 100 / 2;
+  var p2_porcentajeEscena = p2_escena * 100 / 2;
+
+
+
+  // Grafico RADAR
+  var ctx = document.getElementById('graficoRadar').getContext('2d');
+  var chart = new Chart(ctx,{
+        type:"bar",
+        data: {
+          labels:['Patrones', 'Técnica', 'Flow', 'Punch', 'D. tempo', 'P. escena'],
+          datasets: [
+            {
+            label: p1,
+            data: [p1_porcentajePatrones, p1_porcentajeTecnica, p1_porcentajeFlow, p1_porcentajePunch, p1_porcentajeDobletempo, p1_porcentajeEscena],
+            backgroundColor: '#0d47a1',
+            borderColor: 'rgb(0, 98, 255)',
+            borderWidth: 1
+          },{
+            label: p2,
+            data: [p2_porcentajePatrones, p2_porcentajeTecnica, p2_porcentajeFlow, p2_porcentajePunch, p2_porcentajeDobletempo, p2_porcentajeEscena],
+            backgroundColor: '#b71c1c',
+            borderColor: 'rgb(255, 0, 0)',
+            borderWidth: 1
+          }
+        ]},
+        options: {
+          layout: {
+              padding: {
+                  left: 0,
+                  right: 10,
+                  top: 10,
+                  bottom: 10
+              }
+          },
+          legend:{
+            labels:{
+              fontColor: 'white',
+              fontFamily: 'Russo One',
+              display: false
+            }
+          },
+          scales: {
+            xAxes: [{
+
+              ticks: {
+                fontColor: 'white',
+                fontSize: 14,
+                fontFamily: 'Russo One'
+              }
+            }],
+            yAxes: [{
+              display: false,
+              ticks: {
+                beginAtZero: true,
+                min: 0,
+                max: 100,
+                fontSize: 18
+              }
+            }]
+          }
+        }
+
+    });
+
 });
--- js/votacion.js
+++ js/votacion.js
@@ -11,22 +11,51 @@ $(function(){
   function tomarDatos(){
 
     //Participante 1
-    var p1_patron1 = $("#p1_patron1").val();
-    var p1_patron2 = $("#p1_patron2").val();
-    var p1_patron3 = $("#p1_patron3").val();
-    var p1_patron4 = $("#p1_patron4").val();
-    var p1_patron5 = $("#p1_patron5").val();
-    var p1_patron6 = $("#p1_patron6").val();
+    var p1_patron1 = parseInt($("#p1_patron1").val());
+    var p1_patron2 = parseInt($("#p1_patron2").val());
+    var p1_patron3 = parseInt($("#p1_patron3").val());
+    var p1_patron4 = parseInt($("#p1_patron4").val());
+    var p1_patron5 = parseInt($("#p1_patron5").val());
+    var p1_patron6 = parseInt($("#p1_patron6").val());
+    var p1_tecnica = parseInt($("#p1_tecnica").val());
+    var p1_flow = parseInt($("#p1_flow").val());
+    var p1_punch = parseInt($("#p1_punch").val());
+    var p1_dobletempo = parseInt($("#p1_dobletempo").val());
+    var p1_escena = parseInt($("#p1_escena").val());
+    var p1_totalPatrones;
+
     //Participante 2
-    var p2_patron1 = $("#p2_patron1").val();
-    var p2_patron2 = $("#p2_patron2").val();
-    var p2_patron3 = $("#p2_patron3").val();
-    var p2_patron4 = $("#p2_patron4").val();
-    var p2_patron5 = $("#p2_patron5").val();
-    var p2_patron6 = $("#p2_patron6").val();
+    var p2_patron1 = parseInt($("#p2_patron1").val());
+    var p2_patron2 = parseInt($("#p2_patron2").val());
+    var p2_patron3 = parseInt($("#p2_patron3").val());
+    var p2_patron4 = parseInt($("#p2_patron4").val());
+    var p2_patron5 = parseInt($("#p2_patron5").val());
+    var p2_patron6 = parseInt($("#p2_patron6").val());
+    var p2_tecnica = parseInt($("#p2_tecnica").val());
+    var p2_flow = parseInt($("#p2_flow").val());
+    var p2_punch = parseInt($("#p2_punch").val());
+    var p2_dobletempo = parseInt($("#p2_dobletempo").val());
+    var p2_escena = parseInt($("#p2_escena").val());
+    var p2_totalPatrones;
 
+    p1_totalPatrones = p1_patron1 + p1_patron2 + p1_patron3 + p1_patron4 + p1_patron5 + p1_patron6;
+    p2_totalPatrones = p2_patron1 + p2_patron2 + p2_patron3 + p2_patron4 + p2_patron5 + p2_patron6;
 
-    console.log(p1_patron6);
+    //Player 1
+    localStorage.setItem('p1_totalPatrones', p1_totalPatrones);
+    localStorage.setItem('p1_tecnica', p1_tecnica);
+    localStorage.setItem('p1_flow', p1_flow);
+    localStorage.setItem('p1_punch', p1_punch);
+    localStorage.setItem('p1_dobletempo', p1_dobletempo);
+    localStorage.setItem('p1_escena', p1_escena);
+
+    localStorage.setItem('p2_totalPatrones', p2_totalPatrones);
+    localStorage.setItem('p2_tecnica', p2_tecnica);
+    localStorage.setItem('p2_flow', p2_flow);
+    localStorage.setItem('p2_punch', p2_punch);
+    localStorage.setItem('p2_dobletempo', p2_dobletempo);
+    localStorage.setItem('p2_escena', p2_escena);
+
 
   }
 
@@ -36,19 +65,20 @@ $(function(){
       e.preventDefault();
 
       swal({
-        title: "Terminar Batatalla?",
+        title: "¿Terminar  la batalla?",
         text: ""+ p1 + " Vs " + p2 + "",
-        icon: "danger",
+        icon: "warning",
         buttons: true,
         dangerMode: true
       })
       .then((willDelete) => {
         if (willDelete) {
-          swal("Mostrando resultados", {
+          tomarDatos();
+          swal("¡Resultados listos!", {
             icon: "success",
           });
-          tomarDatos();
-          //$(location).attr('href', 'resultados.html');
+
+          $(location).attr('href', 'resultados.html');
         } else {
           e.preventDefault();
           swal("Continua votando..");
@@ -58,6 +88,6 @@ $(function(){
 
     });
 
-  var p1_patron1 = $("#p1_patron1").val();
+
 
 });
